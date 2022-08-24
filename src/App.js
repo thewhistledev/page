30706ea98da418a512bcebfded765addd7dba036
thewhistledev <firewallvmt@gmail.com>
2022-08-24 12:02:15 +0100

    Coming Soon Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import logo from './load.svg';
 import './App.css';
+import React, { useState } from 'react';
+import daysleft from 'daysleft';
+import { usePopper } from 'react-popper';
+
+const timeTillComplete = '05/31/2023';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="" alt="logo" />
+        <h1><span className="eGlowCharacter">U</span><span className="eGlowCharacter2">n</span><span className="eGlowCharacter">d</span><span className="eGlowCharacter2">e</span><span className="eGlowCharacter">r</span> <span className="eGlowCharacter2">C</span><span className="eGlowCharacter">o</span><span className="eGlowCharacter2">n</span><span className="eGlowCharacter">s</span><span className="eGlowCharacter2">t</span><span className="eGlowCharacter">r</span><span className="eGlowCharacter2">u</span><span className="eGlowCharacter">c</span><span className="eGlowCharacter2">t</span><span className="eGlowCharacter">i</span><span className="eGlowCharacter2">o</span><span className="eGlowCharacter">n</span></h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please wait until my website is finished being made!<br/>
+          Estimate of <code className="daysleft">{daysleft(timeTillComplete)}</code> days until the website is completed.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
